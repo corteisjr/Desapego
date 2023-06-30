ec1ea1b05a03d1f59b871d7f2d5c17498f201b52
arcenioCorteis <arcenioCorteis@github.com>
2023-07-01 01:45:42 +0200

    feat: configurations of the project

---
+++ desapego/settings/production.py
@@ -0,0 +1,17 @@
+from .settings import *
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
