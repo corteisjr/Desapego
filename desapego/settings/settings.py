bd162b0e42342731a4a5415cb0088e9e07d9a411
arcenioCorteis <arcenioCorteis@github.com>
2023-07-01 03:25:36 +0200

    feat: Configure static and media funtionality-

--- desapego/settings/settings.py
+++ desapego/settings/settings.py
@@ -117,6 +117,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
