f5bfac50db0a7571eef2c6a48de34cefc26a5043
arcenioCorteis <arcenioCorteis@github.com>
2023-07-03 01:32:51 +0200

    feat: Adjust Login form

--- doeja/forms/AuthForm.py
+++ doeja/forms/AuthForm.py
@@ -4,13 +4,13 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(
         required=True, 
-        widget=forms.TextInput(attrs={'class': 'input-field'}),
+        widget=forms.TextInput(attrs={'class': 'input-field, left-align'}),
         label="Usuário"
         )
     password = forms.CharField(
         max_length=32,
         widget=forms.PasswordInput(
-            attrs={'class': 'input-field'}
+            attrs={'class': 'input-field,left-align'}
         ),
         required=True, 
         label='Senha'
