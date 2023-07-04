cfc1e4630dc95f92412f142a6203588a660196c8
arcenioCorteis <arcenioCorteis@github.com>
2023-07-04 22:49:47 +0200

    feat: create register view

--- doeja/forms/AuthForm.py
+++ doeja/forms/AuthForm.py
@@ -15,6 +15,6 @@ class LoginForm(forms.Form):
     )
     
 class RegisterForm(forms.Form):
-    username = forms.CharField(required=True)
+    username = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'input-field'}))
     email  = forms.CharField(required=True, widget=forms.EmailField(attrs={'class': 'input-field'}))
     password = forms.CharField(max_length=32, widget=forms.PasswordInput(attrs={'class': 'input-field'}))
--- doeja/views/AuthView.py
+++ doeja/views/AuthView.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from doeja.forms.AuthForm import LoginForm
+from django.contrib.auth.models import User
+from doeja.forms.AuthForm import LoginForm, RegisterForm
 
 
 
@@ -42,8 +43,56 @@ def login_view(request):
     )
     
 def register_view(request):
-    ...
+    registerForm = RegisterForm()
+    message = None
     
+    if request.user.is_authenticated:
+        return redirect('/')
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        registerForm = RegisterForm(request.POST)
+        
+        if registerForm.is_valid():
+            # verify that user exist
+            verify_username = User.objects.filter(username=username).first()
+            verify_email = User.objects.filter(email=email).first()
+            
+            if verify_username is not None:
+                message = {
+                    'type' : 'danger',
+                    'text' : 'Já existe um usuário com este username!!'
+                }
+            
+            elif verify_email is not None:
+                message = {
+                    'type': 'danger',
+                    'text' : 'Já existe um usuário com este e-mail!!'
+                }
+            else:
+                user = User.objects.create_user(username, email, password)
+                if user is  not None:
+                    message = {
+                        'type': 'success',
+                        'text': 'Conta criada com sucesso!'
+                    }
+                else:
+                    message = {
+                        'type': 'danger',
+                        'text': 'Um erro ocorreu ao tentar criar a conta'
+                    }
+                    
+    context = {
+        'form' : registerForm,
+        'message': message,
+        'title' : 'Registrar',
+        'button_text' : 'Registrar'
+    }
+    
+    return render(request, template_name='auth/auth.html', context=context, status=200)
+        
 def logout_view(request):
     logout(request)
     return redirect('/login')
