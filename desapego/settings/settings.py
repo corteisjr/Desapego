2ac54fa0f264f59fac34ec6b4ec4aaca6a4c0717
arcenioCorteis <arcenioCorteis@github.com>
2023-07-23 18:53:22 +0200

    feat: Create a donation view

--- desapego/settings/settings.py
+++ desapego/settings/settings.py
@@ -125,3 +125,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_URL = '/logout'
+LOGOUT_REDIRECT_URL = '/login'
--- doeja/views/__init__.py
+++ doeja/views/__init__.py
@@ -1,3 +1,4 @@
 from .HomeView import *
 from .ProfileView import *
 from .AuthView import *
+from .DonationView import *
